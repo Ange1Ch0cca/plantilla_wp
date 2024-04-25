5370c8537802969de06e3626b9146268468e6392
Ange1Ch0cca <angelchoccaramos@gmail.com>
2024-04-24 21:14:47 -0500

    Ultima actualizacion del dia 24/04/2024
    Cambio de parte del Head de todas las hojas por un codigo corto de php para poder modificar desde una sola hoja para todos en general.

--- admin/promociones.php
+++ admin/promociones.php
@@ -2,15 +2,8 @@
 <html lang="es">
 
     <head>
-        <meta charset="utf-8" />
-        <title>Multiarticulos Perú - Administrador</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta content="Panel de Administración de Multiarticulos Perú, tu destino para una amplia variedad de productos." name="description" />
-        <meta content="EvoluTi-Perú" name="AngelChocca" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
+    
+        <?php include ('layout/head.php');?>
 
         <!-- Plugins css -->
         <link href="../plugins/datatables/dataTables.bootstrap4.css" rel="stylesheet" type="text/css" />
